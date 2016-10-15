d5e8c6f25ecfa6b29fb78b39488139b9a82bfe7c
polycomxix <ploy@chargedconcept.com>
2016-10-15 12:51:30 +0900

    Revert "change name to app"
    This reverts commit f88b4d249a4aa3638944586544e08246352a7a93.

--- twitter/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	define('CONSUMER_KEY','JsvyN7xucPtrEwXUVY2ZCB9kg');
-	define('CONSUMER_SECRET','EMUY1Siei47VwNAKPwVuQZrmuMVWUApzHpXOgxBebAZjsiTE6a');
-	define('OAUTH_CALLBACK','http://sns.jin.ise.shibaura-it.ac.jp/app/twitter/oauthcallback.php');
-
-	//user: oyoeoyo
-	//pass: .yoe8@YOE?
-?>
